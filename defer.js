ba95002979e1c90fc3999327aaacf648d0b29771
matthingly-git <matthew.cesar96@gmail.com>
2020-04-06 20:25:23 +0200

    Updare defer non finito

--- defer.js
+++ defer.js
@@ -1,1 +1,5 @@
-
+//Aggiungo a tutte le funzioni il metodo defer usando prototype
+Function.protoype.defer = function(ms) {
+  //defer mi ritorna il numero dato in input e mi ritarda l'esecuzione della funziona a cui la applico con un ritardo di x ms
+  setTimeout(this, ms)
+}
